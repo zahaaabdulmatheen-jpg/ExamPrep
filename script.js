34193bf8eb2d1efc413223b984d541e27e58ca88
zaa <zahaaabdulmatheen@gmail.com>
2026-06-20 13:19:01 +0500

    Refactor toggleMenu function for clarity

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let menuOpen = false;
 
 function toggleMenu() {
-    const sidebar = document.getElementById("sidebar");
 
-    if (menuOpen === false) {
+    const sidebar = document.getElementById("sidebar");
+    const menuBtn = document.getElementById("menuBtn");
+
+    if(menuOpen === false){
+
         sidebar.style.left = "0";
+        menuBtn.innerHTML = "✕";
         menuOpen = true;
-    } else {
-        sidebar.style.left = "-300px";
-        menuOpen = false;
+
     }
+    else{
+
+        sidebar.style.left = "-300px";
+        menuBtn.innerHTML = "☰";
+        menuOpen = false;
+
+    }
+
 }

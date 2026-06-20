645c87ed2cfaafcc08d17cc4575a7a8f235a1d5f
zahaaabdulmatheen-jpg <zahaaabdulmatheen@gmail.com>
2026-06-20 11:31:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function openMenu(){
-
-document.getElementById("sidebar").style.right="0";
-
+document.getElementById("sidebar").style.left="0";
 }
 
 function closeMenu(){
-
-document.getElementById("sidebar").style.right="-300px";
-
+document.getElementById("sidebar").style.left="-300px";
 }
